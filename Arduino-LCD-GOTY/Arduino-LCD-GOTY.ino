34c23c26bbe4af895783100d03f811ea5ed56bcc
Carlos Benito Jareño <carlosbj1230@gmail.com>
2018-09-07 16:48:45 +0200

    Implement Spike Collision and turn Spike Movement into function

--- Arduino-LCD-GOTY/Arduino-LCD-GOTY.ino
+++ Arduino-LCD-GOTY/Arduino-LCD-GOTY.ino
@@ -1,5 +1,10 @@
 #include <LiquidCrystal.h>
 
+void spikeMovement(int *spikeColumn, int spikeRow, int *spikeMoveLoad, int spikeMoveDelay, int *spikeState);
+
+bool spikeCollision(int spikeColumn, int spikeRow, int playerRow);
+
+
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -8,7 +13,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 // game variables
-int gameSpeed = 100;
+float gameSpeed = 16.6666667;	// 60 fps??? i have no idea lmao
 
 // "sprites"
 char player = 'o', spike = '<', clear = ' ';
@@ -17,8 +22,11 @@ char player = 'o', spike = '<', clear = ' ';
 const int buttonPin = 13;
 int buttonState = 0, buttonPrevState = 0;
 
+// player variables
+int playerColumn = 1, playerRow = 1;
+
 // spike variables
-int spikeColumn = 14, spikeRow = 1, spikeMoveLoad = 0, spikeMoveDelay = 5, spikeState = 1;
+int spikeColumn = 14, spikeRow = 1, spikeMoveLoad = 0, spikeMoveDelay = 20, spikeState = 1;
 
 
 
@@ -26,11 +34,10 @@ void setup() {
 
 	pinMode(buttonPin, INPUT);
 	lcd.begin(16, 2);
-	lcd.setCursor(1, 1);
+	lcd.setCursor(playerRow, playerColumn);
 	lcd.print(player);
 	lcd.setCursor(spikeColumn, spikeRow);
 	lcd.print(spike);
-
 }
 
 void loop() {
@@ -42,37 +49,64 @@ void loop() {
 	// player position update
 	if (buttonState == HIGH && buttonPrevState == LOW) {		// jump
 
-		lcd.setCursor(1, 1);
+		lcd.setCursor(playerColumn, playerRow);
 		lcd.print(clear);
-		lcd.setCursor(1, 0);
+		playerRow = 0;
+		lcd.setCursor(playerColumn, playerRow);
 		lcd.print(player);
 
 	}
 	else if (buttonState == LOW && buttonPrevState == HIGH) {	// ground
 
-		lcd.setCursor(1, 0);
+		lcd.setCursor(playerColumn, playerRow);
 		lcd.print(clear);
-		lcd.setCursor(1, 1);
+		playerRow = 1;
+		lcd.setCursor(playerColumn, playerRow);
 		lcd.print(player);
 
 	}
 
 	// spike position update
-	if (spikeMoveLoad++ == spikeMoveDelay && spikeState == 1) {
-		
-		lcd.setCursor(spikeColumn, spikeRow);
-		lcd.print(clear);
-		if (spikeColumn != 0){
+	spikeMovement(&spikeColumn, spikeRow, &spikeMoveLoad, spikeMoveDelay, &spikeState);
 
-			lcd.setCursor(--spikeColumn, spikeRow);
+	// spike collision detection
+	if (spikeCollision(spikeColumn, spikeRow, playerRow))
+		delay(3000);
+
+
+
+	buttonPrevState = buttonState;		// needed in order to avoid unnecessary player position updates
+}
+
+
+
+void spikeMovement(int *spikeColumn, int spikeRow, int *spikeMoveLoad, int spikeMoveDelay, int *spikeState){
+
+	if ((*spikeMoveLoad)++ == spikeMoveDelay && (*spikeState) == 1) {
+		
+		lcd.setCursor((*spikeColumn), spikeRow);
+		lcd.print(clear);
+		if ((*spikeColumn) != 0){
+
+			lcd.setCursor(--(*spikeColumn), spikeRow);
 			lcd.print(spike);
 
 		}
 		else		// when the spike reaches the left edge, it is disabled
-			spikeState = 0;
+			(*spikeState) = 0;
 			
-		spikeMoveLoad = 0;
+		(*spikeMoveLoad) = 0;
 	}
+}
 
-	buttonPrevState = buttonState;		// needed in order to avoid unnecessary player position updates
+
+
+bool spikeCollision(int spikeColumn, int spikeRow, int playerRow){
+
+	bool collision = false;
+
+	if (spikeColumn == 1 && spikeRow == playerRow)
+		collision = true;
+
+	return collision;
 }
